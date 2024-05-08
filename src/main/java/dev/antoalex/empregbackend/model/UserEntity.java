dc24942bd76b56d84005a857d8e4ee70f4595d10
antoalex07 <antoalex710@gmail.com>
2024-05-08 23:35:12 +0530

    joined user and role databases

--- src/main/java/dev/antoalex/empregbackend/model/UserEntity.java
+++ src/main/java/dev/antoalex/empregbackend/model/UserEntity.java
@@ -1,12 +1,12 @@
 package dev.antoalex.empregbackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Document(collection = "users")
@@ -20,4 +20,9 @@ public class UserEntity {
 
     private String userName;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable( name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
 }
